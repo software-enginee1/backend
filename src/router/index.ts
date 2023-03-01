fd480d26796cd0e7b505e605752aa172698e35ae
feiwhang <feiwhang@gmail.com>
2023-03-01 17:53:20 +0700

    Add navigation guards for auth state

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '@/views/LoginView.vue'
 import RegisterView from '@/views/RegisterView.vue'
 import DashboardView from '@/views/DashboardView.vue'
+import { useFirebaseAuth } from 'vuefire'
 
 const routes = [
   {
@@ -24,6 +25,22 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(),
   routes
+})
+
+router.beforeEach((to) => {
+  const publicPages = ['/login', '/register']
+  const authRequired = !publicPages.includes(to.path)
+  const authPrevented = publicPages.includes(to.path)
+  const isLoggedIn = useFirebaseAuth()?.currentUser ?? false
+
+  if (authRequired && !isLoggedIn) {
+    return '/login'
+  }
+
+  if (authPrevented && isLoggedIn) {
+    alert("You're already logged in!")
+    return '/'
+  }
 })
 
 export default router
