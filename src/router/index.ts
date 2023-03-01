0ddfb3b89464982d08f205a3d905892e2a513882
feiwhang <feiwhang@gmail.com>
2023-03-01 15:01:39 +0700

    Migrates from last setup

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,31 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import LoginView from "@/views/LoginView.vue";
+import RegisterView from "@/views/RegisterView.vue";
+import DashboardView from "@/views/DashboardView.vue";
 
-const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
+
+const routes = [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+        path: '/login',
+        name: 'login',
+        component: LoginView
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+        path: '/register',
+        name: 'Register',
+        component: RegisterView
+    },
+    {
+        path: '/',
+        name: 'Dashboard',
+        component: DashboardView
     }
-  ]
+]
+
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
 })
 
 export default router
