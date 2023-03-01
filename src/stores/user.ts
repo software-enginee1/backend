c72b8973cff70fc39db7204452d659aa16b4975d
feiwhang <feiwhang@gmail.com>
2023-03-01 17:37:46 +0700

    Add register logic

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -1,5 +1,8 @@
 import { reactive } from 'vue'
 import { defineStore } from 'pinia'
+import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth'
+import { useFirebaseAuth } from 'vuefire'
+import { useRouter } from 'vue-router'
 
 export const useUserStore = defineStore('user', () => {
   const user = reactive({
@@ -7,14 +10,23 @@ export const useUserStore = defineStore('user', () => {
     isLoggedIn: false
   })
 
+  const router = useRouter()
+  const auth = useFirebaseAuth()
+
   const login = (email: string, password: string) => {
     // Do something with the email and password
     user.isLoggedIn = true
   }
 
-  const register = (name: string, email: string, password: string) => {
-    // Do something with the email, password and name
-    user.isLoggedIn = true
+  const register = async (name: string, email: string, password: string) => {
+    if (!auth) throw new Error('No auth instance found')
+
+    const userCredential = await createUserWithEmailAndPassword(auth, email, password)
+    await updateProfile(userCredential.user, {
+      displayName: name
+    })
+
+    await router.push('/')
   }
 
   return { user, login, register }
