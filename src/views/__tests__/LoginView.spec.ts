4c695377de8610873d3bc0c4873aad987ab91f57
feiwhang <feiwhang@gmail.com>
2023-03-03 10:57:38 +0700

    Fix LoginView test

--- src/views/__tests__/LoginView.spec.ts
+++ src/views/__tests__/LoginView.spec.ts
@@ -1,7 +1,12 @@
-import { describe, it, expect } from 'vitest'
+import { describe, it, expect, beforeEach } from 'vitest'
 
 import { mount } from '@vue/test-utils'
 import LoginView from '@/views/LoginView.vue'
+import { createPinia, setActivePinia } from 'pinia'
+
+beforeEach(() => {
+  setActivePinia(createPinia())
+})
 
 describe('Login', () => {
   it('renders properly', () => {
