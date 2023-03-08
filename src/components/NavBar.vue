1803f2b474a03d1d13f50a55d96a654e88f828ef
feiwhang <feiwhang@gmail.com>
2023-03-08 21:34:48 +0700

    Add responsive hamburger menu

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,9 +2,14 @@
 import { useCurrentUser } from 'vuefire'
 import { logout } from '@/plugins/firebaseAuth'
 import { useRouter } from 'vue-router'
-import { UserCircleIcon } from '@heroicons/vue/24/outline'
+import { UserCircleIcon,  XCircleIcon} from '@heroicons/vue/24/outline'
+import { ref } from "vue";
 
 const router = useRouter()
+const showMobileNav = ref(false)
+router.beforeEach(() => {
+  showMobileNav.value = false;
+});
 
 const user = useCurrentUser()
 const Logout = async () =>
@@ -19,17 +24,18 @@ const Logout = async () =>
 
 <template>
   <div class="bg-indigo-500">
-    <div class="container">
+    <div class="container relative">
       <nav class="flex items-center justify-between flex-wrap py-6">
         <div class="flex items-center flex-shrink-0 text-white mr-6">
           <a class="font-bold text-3xl tracking-tight" href="/">Patreon</a>
         </div>
-        <div class="block lg:hidden">
+        <div class="block md:hidden">
           <button
             class="flex items-center px-3 py-2 border rounded text-indigo-200 border-indigo-400 hover:text-white hover:border-white"
+            @click="showMobileNav = true"
           >
             <svg
-              class="fill-current h-3 w-3"
+              class="fill-current h-4 w-4"
               viewBox="0 0 20 20"
               xmlns="http://www.w3.org/2000/svg"
             >
@@ -38,7 +44,7 @@ const Logout = async () =>
             </svg>
           </button>
         </div>
-        <div class="max-lg:hidden flex items-center gap-8">
+        <div class="max-md:hidden flex items-center justify-center gap-8">
           <router-link to="/" class="link"> Home </router-link>
           <router-link to="/post" class="link" v-if="user"> Post </router-link>
           <router-link to="/profile" v-if="user">
@@ -46,11 +52,29 @@ const Logout = async () =>
           </router-link>
           <div
             @click="user ? Logout() : router.push('/register')"
-            class="inline-block cursor-pointer text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-indigo-500 hover:bg-white mt-4 lg:mt-0"
+            class="inline-block cursor-pointer text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-indigo-500 hover:bg-white mt-4 md:mt-0"
           >
             {{ user ? 'Logout' : 'Register' }}
           </div>
         </div>
+      </nav>
+      <nav v-if="showMobileNav" class="md:hidden absolute top-0 left-0 h-screen pb-6 w-screen bg-indigo-500 flex items-center justify-center">
+        <div class="flex flex-col items-center justify-center gap-8">
+          <router-link to="/" class="link link-mobile"> Home </router-link>
+          <router-link to="/post" class="link link-mobile" v-if="user"> Post </router-link>
+          <router-link to="/profile" v-if="user" class="link link-mobile">
+            Profile
+          </router-link>
+          <div
+            @click="user ? Logout() : router.push('/register')"
+            class="inline-block cursor-pointer text-lg px-6 py-4 leading-none border rounded text-white border-white hover:border-transparent hover:text-indigo-500 hover:bg-white mt-4 md:mt-0"
+          >
+            {{ user ? 'Logout' : 'Register' }}
+          </div>
+        </div>
+         <button @click="showMobileNav = false" class="absolute top-6 right-6 link">
+            <XCircleIcon class="h-8 w-8"/>
+          </button>
       </nav>
     </div>
   </div>
@@ -58,6 +82,9 @@ const Logout = async () =>
 
 <style scoped>
 .link {
-  @apply block mt-4 lg:inline-block lg:mt-0 text-white hover:text-indigo-100;
+  @apply block md:inline-block md:mt-0 text-white hover:text-indigo-100;
+}
+.link-mobile{
+  @apply text-xl;
 }
 </style>
