eb58282ea93925f82e9e185ee108bb0f58578928
feiwhang <feiwhang@gmail.com>
2023-03-08 17:31:18 +0700

    Implement new design on register form

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,96 +1,108 @@
 <script setup>
-import { ref } from 'vue'
-import { useRouter } from 'vue-router'
-import { register } from '@/plugins/firebaseAuth'
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+import { register } from "@/plugins/firebaseAuth";
 
-const name = ref('')
-const email = ref('')
-const password = ref('')
-const error = ref(null)
+const name = ref("");
+const email = ref("");
+const password = ref("");
+const error = ref(null);
 
-const router = useRouter()
+const router = useRouter();
+
+const shouldDisableForm = ref(() => {
+  return name.value === "" || email.value === "" || password.value === "";
+});
 
 const Register = async () => {
   await register(name.value, email.value, password.value)
     .then(() => {
-      error.value = null
-      router.push('/')
+      error.value = null;
+      router.push("/");
     })
     .catch((err) => {
-      error.value = err.message
-    })
+      error.value = err.message;
+    });
 }
 </script>
 
 <template>
-  <div class="container">
-    <div class="row justify-content-center">
-      <div class="col-md-8">
-        <div class="card">
-          <div class="card-header">Register</div>
-          <div class="card-body">
-            <div v-if="error" class="alert alert-danger">{{ error }}</div>
-            <form action="#" @submit.prevent="Register">
-              <div class="form-group row">
-                <label for="name" class="col-md-4 col-form-label text-md-right">Name</label>
-
-                <div class="col-md-6">
-                  <input
-                    id="name"
-                    type="text"
-                    class="form-control"
-                    name="name"
-                    value
-                    required
-                    autofocus
-                    v-model="name"
-                  />
-                </div>
-              </div>
-
-              <div class="form-group row">
-                <label for="email" class="col-md-4 col-form-label text-md-right">Email</label>
-
-                <div class="col-md-6">
-                  <input
-                    id="email"
-                    type="email"
-                    class="form-control"
-                    name="email"
-                    value
-                    required
-                    autofocus
-                    v-model="email"
-                  />
-                </div>
-              </div>
-
-              <div class="form-group row">
-                <label for="password" class="col-md-4 col-form-label text-md-right">Password</label>
-
-                <div class="col-md-6">
-                  <input
-                    id="password"
-                    type="password"
-                    class="form-control"
-                    name="password"
-                    required
-                    v-model="password"
-                  />
-                </div>
-              </div>
-
-              <div class="form-group row mb-0">
-                <div class="col-md-8 offset-md-4">
-                  <button type="submit" class="btn btn-primary">Register</button>
-                </div>
-              </div>
-            </form>
-          </div>
-        </div>
+  <div class="flex flex-col items-center content-center gap-9">
+    <h1 class="text-3xl font-bold">Get Started</h1>
+    <form class="flex flex-col content-center gap-4" action="#" @submit.prevent="Register">
+      <div class="form-group">
+        <label for="name" class="font-bold">Name</label>
+        <input
+          id="name"
+          type="text"
+          class="form-control"
+          name="name"
+          value
+          required
+          autofocus
+          v-model="name"
+        />
       </div>
-    </div>
+
+      <div class="form-group">
+        <label for="email" class="font-bold">Email</label>
+        <input
+          id="email"
+          type="email"
+          class="form-control"
+          name="email"
+          value
+          required
+          autofocus
+          v-model="email"
+        />
+      </div>
+
+      <div class="form-group">
+        <label for="password" class="font-bold">Password</label>
+        <input
+          id="password"
+          type="password"
+          class="form-control"
+          name="password"
+          required
+          v-model="password"
+        />
+      </div>
+
+      <div class="form-group self-center m-5">
+        <button type="submit" class="btn" :disabled="shouldDisableForm()">Register</button>
+      </div>
+    </form>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.form-group {
+  @apply flex flex-col gap-1;
+}
+
+.form-control {
+  @apply outline-none rounded text-black p-2 w-64 transition-shadow;
+}
+
+.form-control:focus {
+  @apply outline-none shadow-lg shadow-indigo-500/50;
+}
+
+.btn {
+  @apply font-bold py-2 px-4 rounded text-white bg-indigo-500 shadow-lg shadow-indigo-500/50 transition-shadow;
+}
+
+.btn:hover {
+  @apply shadow-xl shadow-indigo-500/50;
+}
+
+.btn:disabled {
+  @apply bg-indigo-300 cursor-not-allowed;
+}
+
+.btn:disabled:hover {
+  @apply bg-indigo-300 cursor-not-allowed shadow-lg shadow-indigo-500/50;
+}
+</style>
