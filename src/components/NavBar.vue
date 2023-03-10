4d874713e94bc3572b4dc6e985f1f7eacd18f5cd
feiwhang <feiwhang@gmail.com>
2023-03-10 12:48:38 +0700

    Implement SearchBar UI

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,14 +2,15 @@
 import { useCurrentUser } from 'vuefire'
 import { logout } from '@/plugins/firebaseAuth'
 import { useRouter } from 'vue-router'
-import { UserCircleIcon,  XCircleIcon} from '@heroicons/vue/24/outline'
-import { ref } from "vue";
+import { UserCircleIcon, XCircleIcon } from '@heroicons/vue/24/outline'
+import { ref } from 'vue'
+import SearchBar from "@/components/SearchBar.vue";
 
 const router = useRouter()
 const showMobileNav = ref(false)
 router.beforeEach(() => {
-  showMobileNav.value = false;
-});
+  showMobileNav.value = false
+})
 
 const user = useCurrentUser()
 const Logout = async () =>
@@ -45,6 +46,7 @@ const Logout = async () =>
           </button>
         </div>
         <div class="max-md:hidden flex items-center justify-center gap-8">
+          <SearchBar v-if="user" />
           <router-link to="/" class="link"> Home </router-link>
           <router-link to="/post" class="link" v-if="user"> Post </router-link>
           <router-link to="/profile" v-if="user">
@@ -58,13 +60,15 @@ const Logout = async () =>
           </div>
         </div>
       </nav>
-      <nav v-if="showMobileNav" class="md:hidden absolute top-0 left-0 h-screen pb-6 w-screen bg-indigo-500 flex items-center justify-center">
+      <nav
+        v-if="showMobileNav"
+        class="md:hidden absolute top-0 left-0 h-screen pb-6 w-screen bg-indigo-500 flex items-center justify-center"
+      >
         <div class="flex flex-col items-center justify-center gap-8">
+          <SearchBar v-if="user" class="mb-10" />
           <router-link to="/" class="link link-mobile"> Home </router-link>
           <router-link to="/post" class="link link-mobile" v-if="user"> Post </router-link>
-          <router-link to="/profile" v-if="user" class="link link-mobile">
-            Profile
-          </router-link>
+          <router-link to="/profile" v-if="user" class="link link-mobile"> Profile </router-link>
           <div
             @click="user ? Logout() : router.push('/register')"
             class="inline-block cursor-pointer text-lg px-6 py-4 leading-none border rounded text-white border-white hover:border-transparent hover:text-indigo-500 hover:bg-white mt-4 md:mt-0"
@@ -72,9 +76,9 @@ const Logout = async () =>
             {{ user ? 'Logout' : 'Register' }}
           </div>
         </div>
-         <button @click="showMobileNav = false" class="absolute top-6 right-6 link">
-            <XCircleIcon class="h-8 w-8"/>
-          </button>
+        <button @click="showMobileNav = false" class="absolute top-6 right-6 link">
+          <XCircleIcon class="h-8 w-8" />
+        </button>
       </nav>
     </div>
   </div>
@@ -84,7 +88,7 @@ const Logout = async () =>
 .link {
   @apply block md:inline-block md:mt-0 text-white hover:text-indigo-100;
 }
-.link-mobile{
+.link-mobile {
   @apply text-xl;
 }
 </style>
