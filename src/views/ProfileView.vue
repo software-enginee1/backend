772bc9abfed06adfe2a6176bf90e6baa788594f8
frediej <55649603+frediej@users.noreply.github.com>
2023-03-29 01:51:45 +0700

    While working on the profileView, I noticed that our register will probably need to be changed to automatically create all of the subcollections for newly created users. Pausing profileView to work on that.

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,5 +1,54 @@
-<script setup></script>
+<script>
+import { defineComponent} from 'vue'
+import { useCurrentUser } from 'vuefire'
+// import { db } from '@/plugins/firebaseDB'
+
+
+export default defineComponent({
+  setup() {
+    const user = useCurrentUser();
+    // const userPosts = ref([]);
+    return {
+      user
+    };
+  },
+});
+</script>
+
 
 <template>
-  <div class="container"></div>
+  <div class="white-container">
+    <div>
+
+      <div class='card'>
+        <div class='card-header'>{{ user.displayName }}</div>
+        <div class='card-body'>
+<!--          <div class='bio'> {{ user.metadata.bio }}</div>-->
+          <div class='register-date'>
+            <img src="@/assets/calendar.png" alt="calendar" width="25" height="25">
+            <p> Joined {{ user.metadata.creationTime }} </p>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
 </template>
+
+<style scoped>
+.white-container {
+  background-color: white;
+  color: black;
+  padding: 20px;
+  border-radius: 5px;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+  width: 85%;
+  margin: 0 auto;
+  min-height: 100vh;
+}
+.register-date{
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+}
+</style>
