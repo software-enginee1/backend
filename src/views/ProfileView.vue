2c7467c9f41c7e0a8f69d51a64c5d00fbbba3595
frediej <55649603+frediej@users.noreply.github.com>
2023-03-31 03:24:08 +0700

    Current state of work. Having some issues with database. Apart from that, UI wise nearly everything is complete. Not being able to retrieve things have been impeding the perfection of things. Look at google doc link of full rundown of current issue. https://docs.google.com/document/d/18NgA01ZuNSG9IkfJ5jIz4LE4nuLyVGvTIVUW6RT2SXs/edit?usp=sharing

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,36 +1,162 @@
 <script>
-import { defineComponent} from 'vue'
+import { defineComponent, computed, onMounted, ref } from 'vue'
 import { useCurrentUser } from 'vuefire'
-// import { db } from '@/plugins/firebaseDB'
+import CreatePost from '@/components/CreatePost.vue'
+import { db } from '@/firebase'
+
+// import UserPost from '@/components/UserPost.vue'
+import { collection, query, where, getDocs } from 'firebase/firestore'
 
 
 export default defineComponent({
+  components: { CreatePost },
+  // components: { UserPost },
+  // components: {
+  //   UserPost,
+  // },
+  //
   setup() {
-    const user = useCurrentUser();
-    // const userPosts = ref([]);
+    const user = useCurrentUser()
+    const followerCount = ref(0)
+    const followingCount = ref(0)
+    const userCount = ref(0)
+    const userCount2 = ref(0)
+    let post = ref([])
+
+    const stringJoinedDate = computed(() => {
+      if (user.value && user.value.metadata.creationTime) {
+        return JSON.stringify(user.value.metadata.creationTime).substring(5, 17)
+      }
+      return ''
+    })
+    const userUid = computed(() => {
+      if (user.value && user.value.uid) {
+        return JSON.stringify(user.value.uid).replace(/['"]+/g, '')
+      }
+      return ''
+    })
+
+
+    async function getFollowerCount() {
+      try {
+        const collectionRef = db.collection('post').doc('post1')
+        const doc = await collectionRef.get()
+        if (doc.exists) {
+          console.log('Document data:', doc.data())
+        } else {
+          console.log('No such document!')
+        }
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    async function getFollowingCount() {
+      try {
+        const collectionRef = db.collection('users').doc(userUid).collection('following')
+        const querySnapshot = await collectionRef.get()
+        followingCount.value = querySnapshot.size
+        console.log(followingCount.value)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    // async function getUserCount() {
+    //   const collectionRef = db.collection(db, 'users')
+    //   const querySnapshot = await collectionRef.get()
+    //   if (querySnapshot.empty) {
+    //     console.log('No matching documents.')
+    //     return
+    //   }
+    //   userCount.value = querySnapshot
+    // }
+    //
+    // const getUserCount2 = async () => {
+    //   const collectionRef = db.collection(db, 'users')
+    //   const querySnapshot = await collectionRef.get()
+    //   userCount2.value = querySnapshot.size
+    // }
+
+    // async function fetchUserPosts() {
+    //   const postRef = collection(db, 'post').doc('post1')
+    //   const postDoc = await postRef.get()
+    //   post.value = postDoc.data()
+    //   console.info(post.value)
+    //   console.table(post.value)
+
+    // const q = query(postRef, where('yes', '==', 'yes'))
+    // const postDocs = await getDocs(q)
+    // posts.value = postDocs.docs.map(doc => {
+    //   const data = doc.data()
+    //   return {
+    //     id: doc.id,
+    //     user: data.user,
+    //     content: data.content,
+    //     date: data.date.toDate(),
+    //     likes: data.likes
+    //   }
+    // })
+    // }
+
+    onMounted(() => {
+      getFollowerCount()
+      // getUserCount()
+      // getUserCount2()
+      // fetchUserPosts()
+      getFollowingCount()
+    })
     return {
-      user
-    };
-  },
-});
+      user,
+      stringJoinedDate,
+      userUid,
+      followerCount,
+      userCount,
+      userCount2,
+      post
+      // followingCount
+    }
+  }
+})
 </script>
 
 
 <template>
-  <div class="white-container">
+  <div class='white-container'>
     <div>
 
       <div class='card'>
         <div class='card-header'>{{ user.displayName }}</div>
         <div class='card-body'>
-<!--          <div class='bio'> {{ user.metadata.bio }}</div>-->
+          <!--          <div class='bio'> {{ user.metadata.bio }}</div>-->
           <div class='register-date'>
-            <img src="@/assets/calendar.png" alt="calendar" width="25" height="25">
-            <p> Joined {{ user.metadata.creationTime }} </p>
+            <img src='@/assets/calendar.png' alt='calendar' width='25' height='25'>
+            <p> Joined {{ stringJoinedDate }} </p>
+          </div>
+          <div class='followers'>
+            <h1> {{ userUid }}</h1>
+            <p> {{ followerCount }} Followers {{ follow }}</p>
+            <!--            <p> {{ userCount }} total user count {{ userCount2 }}</p>-->
           </div>
         </div>
       </div>
 
+      <div class='create-post'>
+        <div class='post-box'>
+          <CreatePost />
+        </div>
+      </div>
+
+      <div class='posts'>
+        <!--            <div v-for='post in posts' :key='post.id'>-->
+        <!--              <UserPost-->
+        <!--                :author='post.user'-->
+        <!--                :date='post.date'-->
+        <!--                :content='post.content'-->
+        <!--                :likes='post.likes'-->
+        <!--              />-->
+        <!--            </div>-->
+      </div>
     </div>
   </div>
 </template>
@@ -46,9 +172,11 @@ export default defineComponent({
   margin: 0 auto;
   min-height: 100vh;
 }
-.register-date{
+
+.register-date {
   display: flex;
   align-items: center;
   gap: 1rem;
 }
+
 </style>
