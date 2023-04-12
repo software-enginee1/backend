b9a1480fb9c32467ea8421019ce02c2cdb5712aa
ikkyusan193 <ikkiwie07@gmail.com>
2023-04-12 23:15:28 +0700

    init jest

---
+++ src/plugins/__tests__/firebaseDBSpec.ts
@@ -0,0 +1,43 @@
+// todo.service.spec.ts
+
+import { DBService } from '../firebaseDB'
+
+describe('DBService', () => {
+    let service = new DBService();
+
+    beforeEach(() => {
+        service = new DBService();
+    })
+
+    describe('Setup should Success', () => {
+        test('init', () => {
+            expect(true).toBe(true);
+        })
+    })
+
+    describe('isFollowed should return', () => {
+        test('true', () => {
+            jest.mock('./myClass', () => {
+                return jest.fn().mockImplementation(() => {
+                  return {
+                    isFollowed: jest.fn(() => true),
+                  };
+                });
+              });
+
+            expect(service.isFollowed("test", "test")).toBe(true);
+        })
+
+        test('false', () => {
+            jest.mock('./myClass', () => {
+                return jest.fn().mockImplementation(() => {
+                  return {
+                    isFollowed: jest.fn(() => false),
+                  };
+                });
+              });
+
+              expect(service.isFollowed("test", "test")).toBe(false);
+            })
+    })
+})
